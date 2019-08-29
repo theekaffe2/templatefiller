a553b76507fc9ad8a6d439a11875650418b4916e
theekaffe <sortkaffeogthe@gmail.com>
2019-08-29 17:21:01 +0200

    Changes to makecover

--- makecover.sh
+++ makecover.sh
@@ -9,6 +9,7 @@ posterwidth=250
 maxsize=5099999
 input="$1"
 numberregex='^[0-9]+$'
+output="/tmp/"
 
 if [ -f "$input" ]; then
 	length=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$input")
@@ -20,28 +21,28 @@ else
 fi
 
 if [[ $length =~ $numberregex ]]; then
-if [ ! -d /tmp/grabs$length ]; then mkdir /tmp/grabs$length; fi 
-if [ -f /tmp/grabs$length.txt ]; then rm /tmp/grabs$length.txt; fi
+if [ ! -d "$output"grabs$length ]; then mkdir "$output"grabs$length; fi 
+if [ -f "$output"grabs$length.txt ]; then rm "$output"grabs$length.txt; fi
 
 for ((n=1;n<noframes+1;n++)); do
-	ffmpeg -v error -y -ss $(( length * n / (noframes+1) )) -t $posterlength -i "$1" -vf "fps=$fps,scale=$posterwidth:-1:flags=lanczos" /tmp/grabs$length/"$n.gif"
-	echo "file /tmp/grabs$length/$n.gif" >> /tmp/grabs$length.txt
+	ffmpeg -v error -y -ss $(( length * n / (noframes+1) )) -t $posterlength -i "$1" -vf "fps=$fps,scale=$posterwidth:-1:flags=lanczos" "$output"grabs$length/"$n.gif"
+	echo "file "$output"grabs$length/$n.gif" >> "$output"grabs$length.txt
 done
 
-ffmpeg -hide_banner -loglevel quiet -y -f concat -safe 0 -r 15 -i "/tmp/grabs$length.txt"  -vf "fps=$fps" /tmp/$length.gif
+ffmpeg -hide_banner -loglevel quiet -y -f concat -safe 0 -r $fps -i ""$output"grabs$length.txt"  -vf "fps=$fps" "$output"$length.gif
 
-size=$(stat -c%s "/tmp/$length.gif")
+size=$(stat -c%s ""$output"$length.gif")
 if [ "$size" -gt $maxsize ]&& command -v gifsicless &>/dev/null ; then
-	gifsicle -b --lossy="$compression" /tmp/$length.gif
+	gifsicle -b --lossy="$compression" "$output"$length.gif
 fi
 if [ "$size" -gt $maxsize ]; then
 	echo "Error"
 	>&2 echo "Size of the gif exceeds the jerking limit of 5 mb."
 fi
 
-if [ -d /tmp/grabs$length ]; then rm -r /tmp/grabs$length; fi 
+if [ -d "$output"grabs$length ]; then rm -r "$output"grabs$length; fi 
 
-echo "/tmp/$length.gif"
+echo ""$output"$length.gif"
 exit 0
 
 else
